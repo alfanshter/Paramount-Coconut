0cedee80b78ae7f1ae7a96e6d480f4351f30eca5
Mukhammad Alfan <190710120@students.uajy.ac.id>
2022-01-19 14:38:44 +0700

    perbaikan meta logo

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -10,22 +10,22 @@
     <meta content="Paramount coconuts is a family business that main focused is manufacturing and exporting coconut charcoal, coconut fiber, and coconut peat. We have been exporting since 2015 and keep expanding the business worldwide." name="description" />
     <meta content="charcoal,cocofiber,cocopeat" name="keywords" />
 
-    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('metalogo/apple-icon-57x57.png')}}">
-<link rel="apple-touch-icon" sizes="60x60" href="{{asset('metalogo/apple-icon-60x60.png')}}">
-<link rel="apple-touch-icon" sizes="72x72" href="{{asset('metalogo/apple-icon-72x72.png')}}">
-<link rel="apple-touch-icon" sizes="76x76" href="{{asset('metalogo/apple-icon-76x76.png')}}">
-<link rel="apple-touch-icon" sizes="114x114" href="{{asset('metalogo/apple-icon-114x114.png')}}">
-<link rel="apple-touch-icon" sizes="120x120" href="{{asset('metalogo/apple-icon-120x120.png')}}">
-<link rel="apple-touch-icon" sizes="144x144" href="{{asset('metalogo/apple-icon-144x144.png')}}">
-<link rel="apple-touch-icon" sizes="152x152" href="{{asset('metalogo/apple-icon-152x152.png')}}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{asset('metalogo/apple-icon-180x180.png')}}">
-<link rel="icon" type="image/png" sizes="192x192"  href="{{asset('metalogo/android-icon-192x192.png')}}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{asset('metalogo/favicon-32x32.png')}}">
-<link rel="icon" type="image/png" sizes="96x96" href="{{asset('metalogo/favicon-96x96.png')}}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{asset('metalogo/favicon-16x16.png')}}">
-<link rel="manifest" href="{{asset('metalogo/manifest.json')}}">
+<link rel="apple-touch-icon" sizes="57x57" href="{{asset('img/metalogo/apple-icon-57x57.png')}}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{asset('img/metalogo/apple-icon-60x60.png')}}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{asset('img/metalogo/apple-icon-72x72.png')}}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/metalogo/apple-icon-76x76.png')}}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{asset('img/metalogo/apple-icon-114x114.png')}}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{asset('img/metalogo/apple-icon-120x120.png')}}">
+<link rel="apple-touch-icon" sizes="144x144" href="{{asset('img/metalogo/apple-icon-144x144.png')}}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{asset('img/metalogo/apple-icon-152x152.png')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/metalogo/apple-icon-180x180.png')}}">
+<link rel="icon" type="image/png" sizes="192x192"  href="{{asset('img/metalogo/android-icon-192x192.png')}}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/metalogo/favicon-32x32.png')}}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{asset('img/metalogo/favicon-96x96.png')}}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/metalogo/favicon-16x16.png')}}">
+<link rel="manifest" href="{{asset('img/metalogo/manifest.json')}}">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="{{asset('metalogo/ms-icon-144x144.png')}}">
+<meta name="msapplication-TileImage" content="{{asset('img/metalogo/ms-icon-144x144.png')}}">
 <meta name="theme-color" content="#ffffff">
 
     <!-- Favicons -->
